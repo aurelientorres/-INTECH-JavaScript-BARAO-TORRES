cbb56b34ce77ac1f595d2b5207dc47e06a710d62
Kevin Barao Da Silva <45099131+KevinBarao@users.noreply.github.com>
2021-11-26 17:47:55 +0100

    Added Timer, showbombs, bg sexy

--- demineur.js
+++ demineur.js
@@ -1,8 +1,27 @@
 
 let cells =  [];
 let playing = false;
+let size = 0;
+let firstClick = true;
 
-function createGrid(size, gridId){
+function createGrid(gridId){
+
+    let select = document.getElementById("difficulty")
+    let difficulty = select.options[select.selectedIndex].value;
+
+    switch (difficulty) {
+        case 'easy':
+          size = 10;
+            break;
+        case 'medium':
+            size = 20;
+            break;
+        case 'hard':
+            size = 30;
+          break;          
+        default: 
+            size = 10;
+      }
 
     playing = true
 
@@ -10,6 +29,7 @@ function createGrid(size, gridId){
 
     let table = document.createElement('table');
 
+    grid.innerHTML = "";
     grid.appendChild(table)
 
     for (let i = 0; i < size; i++){
@@ -25,10 +45,28 @@ function createGrid(size, gridId){
             td.style.backgroundColor = "grey";
             tr.appendChild(td);
 
-            td.addEventListener("click", function() {
-                if(playing == true)
-                    checkCell(j, i, size)
-            });
+            td.addEventListener('mousedown', function(e) {
+
+                let cell = cells[i][j]
+                switch (e.button) {
+                    case 0:
+                        if(playing == true  && cell.flag == false)
+                        checkCell(j, i, size)
+                      break;
+                    default:
+
+                    if(cell.discovered == false) {
+                        if(cell.flag == false){
+                            cells[i][j].element.innerHTML ="F"
+                            cells[i][j].flag = true
+                        }
+                        else{
+                            cells[i][j].element.innerHTML =""
+                            cells[i][j].flag = false
+                        }
+                    }
+                }
+            }, false);
 
             
             
@@ -38,7 +76,7 @@ function createGrid(size, gridId){
       
     }
 
-    for(let i = 0; i < size  *  2; i++){
+    for(let i = 0; i < (size * size / 8); i++){
         let randomX = Math.floor(Math.random() * size);
         let randomY = Math.floor(Math.random() * size );
 
@@ -68,12 +106,19 @@ function calculateBomb(size){
 
 }
 function checkCell(x, y, size){
+
+   if(firstClick){
+        firstClick = false;
+        startTimer()
+   }
     
    let cell = cells[y][x] ;
 
    if(cell.bomb == true){
        cell.element.innerHTML = "B"
        playing = false;
+
+       showBombs(size);
 
        return;
    }
@@ -112,12 +157,38 @@ function getCellsAround(x, y, size){
 
 
 class Cell {
-    constructor(x, y, element, adjacentsBomb = 0, discovered = false, bomb = false) {
+    constructor(x, y, element, adjacentsBomb = 0, discovered = false, bomb = false, flag = false) {
       this.x = x;
       this.y = y;
       this.element = element
       this.adjacentsBomb = adjacentsBomb
       this.discovered = discovered
       this.bomb = bomb
+      this.flag = flag
     }
   }
+
+function showBombs(size){
+    for (let i = 0; i < size; i++){
+
+        for(let j = 0; j < size; j++){
+            aroundCells = getCellsAround(j, i, size)
+            let cell = cells[i][j] ;
+            if(cell.bomb == true){
+                cell.element.innerHTML = "B"
+            }
+        }
+    }
+
+}
+
+
+function startTimer() {
+    let counter = 0;
+
+    setInterval(() => {
+        let timer = document.getElementById("time")
+        counter++;
+        timer.innerHTML = counter.toString();
+    },1000)
+}
